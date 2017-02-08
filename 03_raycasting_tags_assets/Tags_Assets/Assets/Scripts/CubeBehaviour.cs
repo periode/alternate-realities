389a2f2f797885f29b95716473236fe4e397663c
Pierre Depaz <pierre.depaz@gmail.com>
2017-02-08 15:12:27 +0400

    added week 3 code

---
+++ 03_raycasting_tags_assets/Tags_Assets/Assets/Scripts/CubeBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour {
+
+	public float redScale;
+	public bool userHasTriggered = false;
+	Vector3 speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = new Vector3 (0.0f, 0.01f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//if this is true
+		if(userHasTriggered){
+
+			//start moving in the direction of the speed variable
+			GetComponent<Transform> ().Translate (speed);
+		}
+
+//		GetComponent<Renderer> ().material.color = new Color((Mathf.Sin(Time.time*redScale)+1)*0.5f, 1.0f, 0.0f);
+	}
+}
