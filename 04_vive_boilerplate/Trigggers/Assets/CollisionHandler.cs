2b6c135b80142535cc5275ed5d74cf7485c6d1cd
Pierre Depaz <pierre.depaz@gmail.com>
2017-02-23 17:13:56 +0400

    inclass code

---
+++ 04_vive_boilerplate/Trigggers/Assets/CollisionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour {
+
+	public GameObject myParticlePrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		//we need an object, a position and a rotation
+//		Instantiate()
+
+		Instantiate (myParticlePrefab, other.transform.position, Quaternion.identity);
+	}
+}
