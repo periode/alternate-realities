500bb4291d85e18138a1ca8b38d6a0a08dfa6e86
Pierre Depaz <pierre.depaz@gmail.com>
2018-01-28 10:27:06 +0400

    added first week lab

---
+++ 01_intro_to_unity/1_InClass/Assets/ProceduralGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGenerator : MonoBehaviour {
+
+	public GameObject myCube;
+	public int numCubes;
+
+	GameObject[] all_cubes;
+
+	// Use this for initialization
+	void Start () {
+		
+		all_cubes = new GameObject[numCubes*numCubes];
+
+
+
+		int k = 0;
+
+		for (int j = 0; j < numCubes; j++) {
+			for (int i = 0; i < numCubes; i++) {
+
+				float newy = Mathf.PerlinNoise (j*0.1f, i*0.1f)*10.0f;
+				all_cubes [k] = GameObject.Instantiate (myCube, new Vector3 (i-numCubes/2, newy, j-numCubes/2), Quaternion.identity);
+				k++;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
