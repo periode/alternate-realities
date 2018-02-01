c80620662063a5110ab4912081e926830fef13e9
Pierre Depaz <pierre.depaz@gmail.com>
2018-02-01 18:55:13 +0400

    added inclass code

---
+++ 02_environments/2_InClass/Assets/FloorReaction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorReaction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col){
+		Debug.Log ("collided with " + col.gameObject.name);
+	}
+
+	void OnCollisionEnter(Collision col){
+		Debug.Log ("collision with " + col.gameObject.name);
+	}
+}
