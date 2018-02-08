e6cf35569c7448eb3d91e57a88af7c7de03e51d1
Shantanu Bhatia <sb5455@nyu.edu>
2018-02-08 17:23:56 +0400

    Added Homework Project
    Note - my HW1 is a subset of my HW1  - it was just a non-rendering
    collider that generated explosions inside of it

---
+++ projects/Shantanu/_vr01/Assets/Scripts/CubeBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour {
+
+
+    public float xpos = 0.0f;
+    public float xspeed = 0.3f;
+	// Use this for initialization
+	void Start () {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+        //GameObject.Find("MuhSphere").GetComponent<MeshRenderer>().material.color = Color.blue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        xpos = transform.position.x;
+        xpos += xspeed;
+
+        GetComponent<MeshRenderer>().material.color = Color.HSVToRGB(Mathf.PerlinNoise(xpos, xpos), 0.8f,0.5f);
+        Debug.Log(Mathf.PerlinNoise(xpos, xpos));
+
+    }
+}
