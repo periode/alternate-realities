71506a56392daaf6bbfd58bc69b89abccd804c83
Pierre Depaz <pierre.depaz@gmail.com>
2018-02-08 16:55:30 +0400

    added sandbox code

---
+++ projects/pierre/Week_3_Sandbox/Assets/Scripts/LerpManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpManager : MonoBehaviour {
+
+	float lerpAmt = 0f;
+	float lerpSpeed = 0.001f;
+
+	Vector3 startPos;
+	Vector3 destPos;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+		destPos = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Linear Interpolation
+		// a value that is at a certain percentage of a distance between two values
+		if (lerpAmt < 1.0f)
+			lerpAmt += lerpSpeed;
+
+		GetComponent<MeshRenderer> ().material.color = Color.Lerp (Color.red, Color.green, lerpAmt);
+
+		destPos.x = Mathf.Sin (Time.time * 0.5f)*3f;
+		transform.position = Vector3.Lerp (startPos, destPos, lerpAmt);
+	}
+}
