d2a39df571a92cdaceb2958a16b66b18915dc67d
Alejandra Trejo <atr313@nyu.edu>
2018-02-12 21:50:43 +0400

    Ale's first project

---
+++ projects/alejandra/01_eyeseeyou/Assets/risas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class risas : MonoBehaviour {
+
+	public AudioSource myAudio;
+
+	public float minimum = 0.1f;
+	public float maximum = 1.0f;
+	public float volumeSpeed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		myAudio = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myAudio.volume = Mathf.Lerp(minimum, maximum, Time.time * volumeSpeed);
+
+	}
+}
