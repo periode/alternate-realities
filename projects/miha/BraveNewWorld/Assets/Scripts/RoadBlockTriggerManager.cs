7b9fb503f1a356cb6d7c8cea57461f961ac2e58e
NYUAD <nyuad@C3-029-MBP-12.local>
2018-02-13 15:55:58 +0400

    added miha's brave_n_world

---
+++ projects/miha/BraveNewWorld/Assets/Scripts/RoadBlockTriggerManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadBlockTriggerManager: MonoBehaviour {
+	public float collision_time = 0.0f;
+	PlayerInteractionManager player;
+
+	void Setup(){
+//		player = GameObject.Find("Player").gameObject.GetComponent<PlayerInteractionManager> ();
+	}
+
+	void OnTriggerEnter(Collider collider){
+//		collision_time = 0;
+//		Debug.Log ("ooops, new visitor, star interaction!");
+		if (collider.gameObject.name == "Player") {
+			Debug.Log ("player just entered!");
+			collider.gameObject.GetComponentInChildren<RaycastManager> ().enabled = true;
+			collider.gameObject.GetComponent<PlayerInteractionManager>().StageConnectionWithParticle (true);
+//
+		}
+	}
+
+//	void OnCollisionStay(){
+//		collision_time += Time.deltaTime;
+//		Debug.Log ("collision time is" +  collision_time);
+//	}
+//
+	void OnTriggerExit(Collider collider){
+		if (collider.gameObject.name == "Player") {
+			Debug.Log ("player just exited!");
+//			collider.gameObject.GetComponentInChildren<RaycastManager> ().enabled = false;
+//			collider.gameObject.GetComponent<PlayerInteractionManager>().StageConnectionWithParticle (false);
+		}
+	}
+}
