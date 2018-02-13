9775071e408a0e6a4c55c9bdc918d0f2f2cfb0bd
Alia ElKattan <aae322@nyu.edu>
2018-02-13 14:33:00 +0400

    add project

---
+++ projects/Alia/01_hope/[A Brave New World]/Assets/Scripts/cameracontroller.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameracontroller : MonoBehaviour
+{
+
+    //public float speed;
+    float tunnelx;
+    public GameObject tunnel;
+    float speed = 10;
+    void Update()
+
+    {
+        int x = 0;
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+            x++;
+            if (x%10==0)
+            {
+                speed += 2;
+            }
+           
+           
+
+        }
+    }
+
+   
+}
+
+
+
+
