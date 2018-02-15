74f9e1447cc5242743aba62fc38a25c6c19ac1b1
Alia ElKattan <aae322@nyu.edu>
2018-02-15 14:42:13 +0400

    added my project

---
+++ projects/Alia/01_hope/Assets/Scripts/raycastmanager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycastmanager : MonoBehaviour
+{
+    public GameObject player;
+    public int raycastDistance;
+    public LayerMask layers;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Raycast();
+
+    }
+
+    void Raycast()
+    {
+         float speed = 10;
+        int x = 0;
+        Vector3 forward = transform.forward;
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, forward, out hit, raycastDistance, layers))
+        {
+            //Debug.Log("raycast hit!");
+            player.transform.Translate(new Vector3(speed * Time.deltaTime, 0,0));
+            x++;
+            if (x % 10 == 0)
+            {
+                speed += 2;
+            }
+
+
+        }
+    }
+}
---
+++ projects/Alia/01_hope/Assets/Scripts/triggerenter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+using System.Collections;
+
+
+public class triggerenter : MonoBehaviour
+{
+    public GameObject tunnel;
+
+    //GameObject[] all_tunnels; 
+    //float tunnelx = 46;
+    
+
+    void OnTriggerEnter(Collider other)
+    {
+        float thisx = this.transform.position.x; //-10.79.. 39.346
+       
+        if (other.gameObject.tag == "collider")
+        {
+            GameObject.Find("Environment").GetComponent<ProceduralGenerator>().triggered(other, thisx); 
+        }
+
+    }
+
+
+}
