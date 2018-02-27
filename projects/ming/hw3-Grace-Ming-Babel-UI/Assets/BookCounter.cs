2af6c1828a6840817f8e978f35ac492b917bc55d
mingwho <mh3833@nyu.edu>
2018-02-27 02:58:41 -0800

    Added hw3 Babel library with UI

---
+++ projects/ming/hw3-Grace-Ming-Babel-UI/Assets/BookCounter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+using UnityEngine.UI;
+
+public class BookCounter : MonoBehaviour {
+
+	VRTK_InteractableObject bookInteraction;
+	private bool Grabbed = false;
+	private bool pGrabbed = false;
+	public Text bookCover;
+	public Text bookSpine;
+
+	// Use this for initialization
+	void Start () {
+		bookInteraction = this.GetComponent<VRTK_InteractableObject>();
+		bookCover.text = GetComponent<BookContents> ().bookTitle;
+		bookSpine.text = GetComponent<BookContents> ().bookTitle;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 Grabbed = bookInteraction.IsGrabbed();
+
+		if (pGrabbed == false && Grabbed == true) {
+			BooksGrabbedCount.booksGrabbed++;
+			pGrabbed = true;
+		} 
+
+	}
+}
---
+++ projects/ming/hw3-Grace-Ming-Babel-UI/Assets/BooksGrabbedCount.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BooksGrabbedCount : MonoBehaviour {
+	public static int booksGrabbed = 0;
+	public Text countText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SetCountText ();
+		
+	}
+	void SetCountText ()
+	{
+		countText.text = "You grabbed " + booksGrabbed.ToString () + " books";
+
+	}		
+}
