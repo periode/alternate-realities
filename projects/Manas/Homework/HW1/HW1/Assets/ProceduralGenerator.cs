981250283a4b4c66ebeff92b620b658709c8ee2f
Manas Pant <ManasPant@Manass-MacBook-Pro.local>
2018-03-01 15:46:57 +0400

    removed submodule

---
+++ projects/Manas/Homework/HW1/HW1/Assets/ProceduralGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGenerator : MonoBehaviour {
+    
+    public List<GameObject> points = new List<GameObject>();
+
+    public GameObject myPrefab;
+    public GameObject myPrefab1;
+    public GameObject myPrefab2;
+
+
+	// Use this for initialization
+	void Start () {
+        points.Add(myPrefab as GameObject);
+        points.Add(myPrefab1 as GameObject);
+        points.Add(myPrefab2 as GameObject);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int index;
+        index = Random.Range(0, 3);
+        
+        Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));
+        GameObject.Instantiate(points[index], position, Quaternion.identity);
+
+	}
+}
---
+++ projects/Manas/Homework/HW1/README.md
@@ -0,0 +1,2 @@
+# VR
+Repository for my Spring 2018 class "Alternate Realities"
