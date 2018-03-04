0b632c93062bf4acd3b08b7bae24063c5a2de6de
Kotavy <tavius.koktavy@gmail.com>
2018-03-04 20:44:31 +0400

    Basic emotion combine/repel scripts

---
+++ projects/tavius/02_EmergingEmotion/Assets/TODO.cs
@@ -0,0 +1,8 @@
+﻿
+
+/* BUG:
+If both spheres have the script on, both get deleted. Proposed solution: Check 'Is Trigger' to sphere in right hand when holding two emotions.
+
+Decide on orb size after user testing, modularize code to work with it. (See mergeDetection.cs notes)
+
+*/
